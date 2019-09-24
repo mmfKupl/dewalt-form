14bd509982d500e01a006c001b38e4f70816d527
Ivan Kuplevich <ikuplevich97@gmail.com>
2019-09-24 21:44:29 +0300

    initial heroku

--- server.js
+++ server.js
@@ -4,10 +4,10 @@ const app = express();
 
 app.use(express.static(`${__dirname}/dist/dewalt-form`));
 
-app.get('/', (req, res) => {
-  console.log('??');
+app.get('/*', (req, res) => {
   res.sendFile('/dist/dewalt-form/index.html', { root: __dirname });
 });
+
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
   console.log(__dirname);
