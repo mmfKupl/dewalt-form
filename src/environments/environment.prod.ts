9be800532a4dcfd31150b9615ce4eedaa0dfa7b3
Ivan Kuplevich <ikuplevich97@gmail.com>
2019-10-22 23:47:06 +0300

    change environment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,13 @@
-import fitebaseConf from './firebase.config';
 export const environment = {
   production: true,
-  firebase: fitebaseConf
+  firebase: {
+    apiKey: 'AIzaSyAtkSmf8rtF-PdEM1jkTyyQeBMivBIepq4',
+    authDomain: 'dewalt-up18-data.firebaseapp.com',
+    databaseURL: 'https://dewalt-up18-data.firebaseio.com',
+    projectId: 'dewalt-up18-data',
+    storageBucket: 'gs://dewalt-up18-data.appspot.com/',
+    messagingSenderId: '537001885799',
+    appId: '1:537001885799:web:ea2a9175dff2fa31b214c2',
+    measurementId: 'G-PG9ML38XMV'
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,11 +2,18 @@
 // `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
 // The list of file replacements can be found in `angular.json`.
 
-import fitebaseConf from './firebase.config';
-
 export const environment = {
   production: false,
-  firebase: fitebaseConf
+  firebase: {
+    apiKey: 'AIzaSyAtkSmf8rtF-PdEM1jkTyyQeBMivBIepq4',
+    authDomain: 'dewalt-up18-data.firebaseapp.com',
+    databaseURL: 'https://dewalt-up18-data.firebaseio.com',
+    projectId: 'dewalt-up18-data',
+    storageBucket: 'gs://dewalt-up18-data.appspot.com/',
+    messagingSenderId: '537001885799',
+    appId: '1:537001885799:web:ea2a9175dff2fa31b214c2',
+    measurementId: 'G-PG9ML38XMV'
+  }
 };
 
 /*
