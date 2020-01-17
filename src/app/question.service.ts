9734e4f5832a2c6397890e6a0f1031fdd75a8b08
Ivan Kuplevich <ikuplevich97@gmail.com>
2020-01-17 22:45:03 +0300

    delete mock data

--- src/app/question.service.ts
+++ src/app/question.service.ts
@@ -8,59 +8,59 @@ import downloadingAddressQuestions from './questions/downloadingAddressQuestions
 @Injectable({ providedIn: 'root' })
 export class QuestionService {
   senderAnswer: any = {
-    'company-name': null,
-    email: 'ikuplevich97@gmail.com',
-    'name-contact': 'qweqwe',
-    'name-fio': 'qweqwe',
-    'sender-unp': null,
-    'tel-sender': '123123',
-    'type-sender': 'Физ. лицо',
-    'viber-tel': '13'
+    // 'company-name': null,
+    // email: 'ikuplevich97@gmail.com',
+    // 'name-contact': 'qweqwe',
+    // 'name-fio': 'qweqwe',
+    // 'sender-unp': null,
+    // 'tel-sender': '123123',
+    // 'type-sender': 'Физ. лицо',
+    // 'viber-tel': '13'
   };
   addressAnswer: any = {
-    city: 'Минск',
-    'da-dinner-time-from': '12:21',
-    'da-dinner-time-to': '12:12',
-    'da-downloading-time-from': '12:12',
-    'da-downloading-time-to': '12:01',
-    house: '50',
-    'house-building': '2',
-    'house-room': '122',
-    street: 'Тикоцкого',
-    'street-type': 'Бульвар'
+    // city: 'Минск',
+    // 'da-dinner-time-from': '12:21',
+    // 'da-dinner-time-to': '12:12',
+    // 'da-downloading-time-from': '12:12',
+    // 'da-downloading-time-to': '12:01',
+    // house: '50',
+    // 'house-building': '2',
+    // 'house-room': '122',
+    // street: 'Тикоцкого',
+    // 'street-type': 'Бульвар'
   };
   toolsAnswer: any[] = [
-    {
-      'accums-numbers': [],
-      'brend-type': 'Black & Decker',
-      'charger-number': null,
-      'power-type': 'Сетевой',
-      'tool-description': '123sssdsdsdsd',
-      'tool-seller-name': '123',
-      'tool-serial-number': '123',
-      'tool-type': '123',
-      'tool-vendor-code': '123'
-    },
-    {
-      'accums-numbers': ['123123', 'zxczxc', '12312'],
-      'brend-type': 'DeWALT',
-      'charger-number': '123123',
-      'power-type': 'Аккумуляторный',
-      'tool-description': '123123123123132',
-      'tool-seller-name': '123123',
-      'tool-serial-number': '123123',
-      'tool-type': '123123',
-      'tool-vendor-code': '123123'
-    }
+    // {
+    //   'accums-numbers': [],
+    //   'brend-type': 'Black & Decker',
+    //   'charger-number': null,
+    //   'power-type': 'Сетевой',
+    //   'tool-description': '123sssdsdsdsd',
+    //   'tool-seller-name': '123',
+    //   'tool-serial-number': '123',
+    //   'tool-type': '123',
+    //   'tool-vendor-code': '123'
+    // },
+    // {
+    //   'accums-numbers': ['123123', 'zxczxc', '12312'],
+    //   'brend-type': 'DeWALT',
+    //   'charger-number': '123123',
+    //   'power-type': 'Аккумуляторный',
+    //   'tool-description': '123123123123132',
+    //   'tool-seller-name': '123123',
+    //   'tool-serial-number': '123123',
+    //   'tool-type': '123123',
+    //   'tool-vendor-code': '123123'
+    // }
   ];
   departureAnswer: any = {
-    'amount-of-boxes': '2',
-    'd-height': '12',
-    'd-length': '12',
-    'd-weight-total': '12',
-    'd-width': '12',
-    pass: true,
-    'power-of-attorney': true
+    // 'amount-of-boxes': '2',
+    // 'd-height': '12',
+    // 'd-length': '12',
+    // 'd-weight-total': '12',
+    // 'd-width': '12',
+    // pass: true,
+    // 'power-of-attorney': true
   };
 
   links = [
